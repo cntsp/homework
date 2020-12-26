842de5317562912a2a2c4843706052b9d86c6217
cntsp <tsp2090879981@163.com>
2020-12-26 12:45:38 +0800

    程序启动脚本代码入口

--- day8/bin/start.py
+++ day8/bin/start.py
@@ -6,3 +6,9 @@ import sys
 
 config_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(config_path)
+
+from core import main
+
+hm1 = main.HostManager()
+main.HostManager.read_config()
+
